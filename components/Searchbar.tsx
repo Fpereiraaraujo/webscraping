ee838f6e3640496b55f9c9260b742a0116c10ba4
Fernando pereira <fernandomendespereira@hotmail.com>
2023-11-22 10:47:17 -0300

    init project

---
+++ components/Searchbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Searchbar = () => {
+    
+  return (
+    <form className='flex flex-wrap gap-4 mt-12'>
+        <input type="text"
+        value={}
+        onChange={}
+        placeholder='Enter Product Link'
+        className='' />
+
+    </form>
+  )
+}
+
+export default Searchbar
